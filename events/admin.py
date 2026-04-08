0b0842f98c74e12224db1b1066b30e194f4c8498
xeclat <akwisame@gmail.com>
2026-04-08 13:54:45 +0200

    registering models with admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Club, Event, TicketType, Registration
 
-# Register your models here.
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('name', 'president', 'contact_email', 'is_approved')
+    list_filter = ('is_approved',)
+    search_fields = ('name', 'president__username')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'hosting_club', 'date_and_time', 'location', 'max_capacity')
+    list_filter = ('hosting_club', 'date_and_time')
+    search_fields = ('title', 'location')
+
+@admin.register(TicketType)
+class TicketTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event', 'price', 'quantity_available')
+    list_filter = ('event',)
+
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('attendee', 'event_ticket', 'registration_date', 'has_attended')
+    list_filter = ('has_attended', 'event_ticket__event')
+    search_fields = ('attendee__username',)
