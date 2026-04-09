67a5d868b53ad78256b1ae434925bac82569cd45
reshma <reshma24799@gmail.com>
2026-04-09 11:06:30 +0200

    urls and navbar of clubhub

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name = 'events'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('event/<int:event_id>/', views.event_detail, name='event_detail'),
+    path('my-tickets/', views.my_tickets, name='my_tickets'),
+    path('unregister/<int:registration_id>/', views.unregister, name='unregister'),
+    path('signup/', views.signup, name='signup'),
 ]
